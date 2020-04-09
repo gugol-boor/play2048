2c54607f31de6f3f8b646e4889d6c2bbb69a1680
code7s <code7s@qq.com>
2020-04-09 12:07:17 +0800

    实现基本功能，待完善分数统计和游戏结束的判断

--- js/2048.js
+++ js/2048.js
@@ -16,27 +16,30 @@ let arr=[2,0,4,2]
     如果不同：把前面那项push到新数组中比较后面的
     以此类推
 得出新数组后有可能length不足4;我们自动在后面添加项
+判断游戏结束：当页面空白格子为0，且所有格子都不能继续合并时游戏失败
 */
 
 let wrap = document.getElementById('wrap');
 // 获取每一个格子
 let obox = wrap.getElementsByTagName('div');
-// 给每一个格子添加下标
-for (let i = 0; i < obox.length; i++) {
-  obox[i].setAttribute('index', i)
-  obox[i].setAttribute('value', 0)
-}
 // 模拟数组
-let arr = [
-  [2, 4, 0, 2],
-  [2, 4, 2, 2],
-  [4, 0, 0, 2],
-  [0, 2, 0, 0]
-];
+// let arr = [
+//   [2, 4, 0, 2],
+//   [2, 4, 2, 2],
+//   [4, 0, 0, 2],
+//   [0, 2, 0, 0]
+// ];
 
+// 初始化：在格子中随机生成一个数a
+(function init(){
+  let n = Math.floor(Math.random() * obox.length)
+  obox[n].className='num2';
+  obox[n].setAttribute('value',2)
+})()
 
-function toleft(arr) {
-  let newarr = []
+// 核心算法
+function toMove(arr) {
+  let newArr = []
   for (var i = 0; i < arr.length; i++) {
     //d1!==0
     if (arr[i] !== 0) {
@@ -49,43 +52,111 @@ function toleft(arr) {
       }
       //d1==d2
       if (arr[i] === arr[j]) {
-        newarr.push(arr[i] + arr[j])
+        newArr.push(arr[i] + arr[j])
         i = j
       } else {// d1!==d2
-        newarr.push(arr[i])
+        newArr.push(arr[i])
       }
     }
   }
   // 新数组比原有数组少几个数我们push几个0进去，保证长度为4
-  var zero = arr.length - newarr.length
+  var zero = arr.length - newArr.length
   for (var i = 0; i < zero; i++) {
-    newarr.push(0)
+    newArr.push(0)
   }
-  return newarr
+  return newArr
 }
-// console.log(toleft(arr));
 
-
-
-
+function run(arr) {
+  // 获取每一排数组的集合
+  let val = [
+    Number(obox[arr[0]].getAttribute('value')),
+    Number(obox[arr[1]].getAttribute('value')),
+    Number(obox[arr[2]].getAttribute('value')),
+    Number(obox[arr[3]].getAttribute('value'))
+  ]
+  // console.log(val);
+  // 得到计算后的数组
+  var newVal = toMove(val)
+  // 把计算后数组里的数字添加到value中并且同步class
+  for (var i = 0; i < arr.length; i++) {
+    obox[arr[i]].setAttribute('value', newVal[i])
+    obox[arr[i]].className = "num" + obox[arr[i]].getAttribute('value')
+  }
+}
+// 移动过后给空白页随机添加数字2
+function addZero() {
+  var nullArr = []
+  // 把所有空白的格子下标添加到新数组中
+  for (var i = 0; i < obox.length; i++) {
+    if (obox[i].getAttribute('value') === '0') {
+      nullArr.push(i)
+    }
+  }
+  // 新数组随机生成一个下标
+  var n = Math.floor(Math.random() * nullArr.length)
+  // console.log(n);
+  // 当新数组有值时随机给空白盒子添加class和value
+  if (nullArr.length !== 0) {
+    obox[nullArr[n]].className = "num2"
+    obox[nullArr[n]].setAttribute('value', 2)
+  }
+}
 document.onkeydown = function (e) {
-  console.log(e.keyCode);
+  // console.log(e.keyCode);
   // W:87 A:65 S:83 D:68
   switch (e.keyCode) {
-    case 65:
-      // 向左移动
-      console.log(toleft(arr[1]));
 
+    // 向左移动
+    case 65:
+      // 第一排
+      run([0, 1, 2, 3])
+      // 第二排
+      run([4, 5, 6, 7])
+      // 第三排
+      run([8, 9, 10, 11])
+      // 第四排
+      run([12, 13, 14, 15])
+      addZero()
       break
+
+    // 向右移动
     case 68:
-      // 向右移动
+      // 第一排
+      run([3, 2, 1, 0])
+      // 第二排
+      run([7, 6, 5, 4])
+      // 第三排
+      run([11, 10, 9, 8])
+      // 第四排
+      run([15, 14, 13, 12])
+      addZero()
       break
+
+    // 向上移动
     case 87:
-      // 向上移动
+      // 第一列
+      run([0, 4, 8, 12])
+      // 第二列
+      run([1, 5, 9, 13])
+      // 第三列
+      run([2, 6, 10, 14])
+      // 第四列a
+      run([3, 7, 11, 15])
+      addZero()
       break;
+
+    // 向下移动
     case 83:
-      // 向下移动
+      // 第一列
+      run([12, 8, 4, 0])
+      // 第二列
+      run([13, 9, 5, 1])
+      // 第三列
+      run([14, 10, 6, 2])
+      // 第四列
+      run([15, 11, 7, 3])
+      addZero()
       break
   }
-
 }
